2773625264d67adef447bb840ed2667f7a73a3fc
magiearavenar <wandacorazonfeliz@gmail.com>
2025-10-27 22:24:36 -0300

    Fix S3 URL generation

--- tienda/settings.py
+++ tienda/settings.py
@@ -103,13 +103,13 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'tienda/static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# AWS S3 Configuration - TEMPORARILY DISABLED
-if False and os.environ.get('AWS_ACCESS_KEY_ID'):
+# AWS S3 Configuration
+if os.environ.get('AWS_ACCESS_KEY_ID'):
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-1')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
     AWS_DEFAULT_ACL = 'public-read'
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
