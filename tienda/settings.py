2913ef3a98127f99dbe4b1c1d255bd3e29760aba
magiearavenar <wandacorazonfeliz@gmail.com>
2025-11-06 00:39:13 -0300

    Fix: Activar context_processor del footer para mostrar redes sociales y footer completo en el frontend

--- tienda/settings.py
+++ tienda/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # 'productos.context_processors.footer_context',  # Desactivado temporalmente
+                'productos.context_processors.footer_context',
             ],
         },
     },
