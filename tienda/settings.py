d358109b8f8db531f31bd2b8bbcd450c2214c43f
magiearavenar <wandacorazonfeliz@gmail.com>
2026-06-05 22:27:07 -0400

    feat: configuracion email Gmail para recuperar contrasena

--- tienda/settings.py
+++ tienda/settings.py
@@ -326,3 +326,13 @@ GOOGLE_MERCHANT_CREDENTIALS = os.environ.get('GOOGLE_MERCHANT_CREDENTIALS', '') 
 
 # Instagram API
 INSTAGRAM_ACCESS_TOKEN = os.environ.get('INSTAGRAM_ACCESS_TOKEN', '')
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER', '')
+SERVER_EMAIL = os.environ.get('EMAIL_HOST_USER', '')
