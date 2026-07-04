8e7939f4cdc9225f6f95068ff8277af1341f4918
magiearavenar <wandacorazonfeliz@gmail.com>
2026-07-04 16:24:05 -0400

    fix: MercadoPago sandbox default False

--- tienda/settings.py
+++ tienda/settings.py
@@ -316,7 +316,7 @@ FLOW_SECRET_KEY = os.environ.get('FLOW_SECRET_KEY', '')
 FLOW_SANDBOX = os.environ.get('FLOW_SANDBOX', 'True').lower() == 'true'
 
 MERCADOPAGO_ACCESS_TOKEN = os.environ.get('MERCADOPAGO_ACCESS_TOKEN', '')
-MERCADOPAGO_SANDBOX = os.environ.get('MERCADOPAGO_SANDBOX', 'True').lower() == 'true'
+MERCADOPAGO_SANDBOX = os.environ.get('MERCADOPAGO_SANDBOX', 'False').lower() == 'true'
 
 SITE_URL = os.environ.get('SITE_URL', 'http://127.0.0.1:8000')
 
