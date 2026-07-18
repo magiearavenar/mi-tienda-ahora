296ae6113033a5c179e29f0bbd8a95f072efee93
magiearavenar <wandacorazonfeliz@gmail.com>
2026-07-18 14:01:23 -0400

    fix: limpiar emojis del feed de Google Merchant

--- google_merchant/feed.py
+++ google_merchant/feed.py
@@ -1,6 +1,17 @@
+import re
 from django.http import HttpResponse
 from django.conf import settings
 from productos.models import Producto
+
+
+def limpiar_texto(texto):
+    """Elimina emojis y caracteres especiales no compatibles con XML."""
+    if not texto:
+        return ''
+    # Eliminar emojis y símbolos
+    texto = re.sub(r'[^\x00-\x7F\xC0-\xFF\u00C0-\u024F\u0400-\u04FF\s]', '', str(texto))
+    # Limpiar espacios extra
+    return texto.strip()
 
 
 def google_feed_xml(request):
@@ -28,13 +39,13 @@ def google_feed_xml(request):
 
         item = f'''    <item>
       <g:id>{p.id}</g:id>
-      <g:title><![CDATA[{p.nombre}]]></g:title>
-      <g:description><![CDATA[{p.descripcion[:5000]}]]></g:description>
+      <g:title><![CDATA[{limpiar_texto(p.nombre)}]]></g:title>
+      <g:description><![CDATA[{limpiar_texto(p.descripcion[:5000])}]]></g:description>
       <g:link>{base_url}/producto/{p.slug}/</g:link>
       <g:image_link>{imagen_url}</g:image_link>
       <g:availability>{disponibilidad}</g:availability>
       <g:price>{p.precio} CLP</g:price>
-      <g:brand><![CDATA[{p.marca}]]></g:brand>
+      <g:brand><![CDATA[{limpiar_texto(p.marca)}]]></g:brand>
       <g:mpn>{mpn}</g:mpn>
       <g:condition>{p.condicion}</g:condition>
       <g:shipping>
@@ -44,7 +55,7 @@ def google_feed_xml(request):
       </g:shipping>'''
 
         if p.google_category:
-            item += f'\n      <g:google_product_category><![CDATA[{p.google_category}]]></g:google_product_category>'
+            item += f'\n      <g:google_product_category><![CDATA[{limpiar_texto(p.google_category)}]]></g:google_product_category>'
 
         # Imágenes adicionales
         for img in p.imagenes.all()[:10]:
